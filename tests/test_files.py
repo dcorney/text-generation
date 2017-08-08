bf8ddc56d6c0a6b039b273907d47a4d0cd65ae5c
David Corney <david.corney@signal.uk.com>
2017-08-08 15:35:16 +0100

    Refactoring S3 writes

---
+++ tests/test_files.py
@@ -0,0 +1,15 @@
+from context import files
+import random
+
+# Include a randon number so that multiple runs of the
+# test don't read the version of the file previously written
+test_obj={"title":"This is the title",
+          "text":"this is the content",
+          "id":random.randint(0,999999)}
+
+def test_s3_write():
+    cloud = files.files(files.Storage_type.s3)
+    cloud.write_s3(test_obj, "test_obj.json")
+    froms3_obj = cloud.read_s3("test_obj.json")
+    assert froms3_obj == test_obj
+
