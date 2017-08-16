e2c396706f002bb438dd6e5dc88b89198628d04d
David Corney <david.corney@signal.uk.com>
2017-08-16 22:06:22 +0100

    Tidying format

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import logging
 
 import core.markovchain as mc
 import core.sentence as sentence
-#from gutenberg import basic_strip as bs
+from gutenberg import basic_strip as bs
 import database.text_importer as ti
 import database.files as store
 import core.dialogue as dialogue
 import knowledge.wikipedia as wiki
 import knowledge.word_vectors as w2v
 import knowledge.names as names
-
+import nlp.tokenizer_stanford as tokenize
 
 LOGDIR = "logs"
 if not os.path.exists(LOGDIR):
@@ -22,30 +22,30 @@ if not os.path.exists(LOGDIR):
 logger = logging.getLogger(__name__)
 
 
-FORMAT = '%(asctime)s %(name)12s %(funcName)12s() %(levelname)7s: %(message)s'
+FORMAT = '%(asctime)s %(name)12ss %(funcName)12s() %(levelname)7s: %(message)s'
 logging.basicConfig(filename=LOGDIR + '/textgen.log', level=logging.DEBUG, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
 logger.info("\n========================================== New run ==========================================")
 
 
-# def load_text():
-#     mcW = mc.MarkovChain()
+def load_text():
+    mcW = mc.MarkovChain()
 #     mcW.delete_all_in_redis_careful()
 
-#     doc = bs.get_clean_text(103)
-#     cache = store.files(store.Storage_type.local_dev)
-#     cache.write_text(doc['text'],"103.txt")
-#     with open("database/resources/texts/103.txt") as file_in:
-#         text = file_in.read()
+    doc = bs.get_clean_text(107)
+    cache = store.files(store.Storage_type.local_dev)
+    cache.write_text(doc['text'], "107.txt")
+    with open("database/resources/texts/107.txt") as file_in:
+        text = file_in.read()
 
-#     tokens = tokenize.tokenize(text[2000:20000])
-#     print(tokens['tokens'][0:50])
-#     print(tokens['entities'])
+    tokens = tokenize.tokenize(text[2000:3000])
+    print(tokens['tokens'][0:50])
+    print(tokens['entities'])
 #     mc.train_words(tokens['tokens'])
 #     mc.append_ner(tokens['entities'])
 
-#     generator = sentence.SentenceMaker(mc)
-#     s = generator.generate_sentence_tokens(["the", "man"])
-#     print(" ".join(s))
+    generator = sentence.SentenceMaker(mc)
+    s = generator.generate_sentence_tokens(["the", "man"])
+    print(" ".join(s))
 
 
 def dev():
