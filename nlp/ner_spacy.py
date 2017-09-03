bf93bf1aea6346d2a785300dacf0bd21f4f1d077
David Corney <david.corney@signal.uk.com>
2017-09-03 17:08:06 +0100

    Removed adjacent pairs of identical NER tokens

--- nlp/ner_spacy.py
+++ nlp/ner_spacy.py
@@ -15,12 +15,14 @@ def ner(text, types=["<PERSON>", "<ORGANIZATION>", "<LOCATION>"]):
     requested types (and the text of other tokens). """
     doc = nlp(" ".join(text.split()))
 
-    # TODO: merge adjacent tokens of same type. E.g. [<PERSON>, <PERSON>] -> [<PERSON>]. I.e. just delete second one of each pair.
+    # merge adjacent tokens of same type. E.g. [<PERSON>, <PERSON>] -> [<PERSON>]. I.e. just delete second one of each pair.
     entities = [{normalize_types.get(e.label_, e.label_): e.text} for e in doc.ents if normalize_types.get(e.label_, e.label_) in types]
     tokens = [normalize_types.get(t.ent_type_, t.text) for t in doc]
-    return {"tokens": tokens, "entities": entities}
+    tokens_filtered = [token for idx, token in enumerate(tokens)
+                       if idx == 0 or (token != tokens[idx - 1] and token[0] == "<")]
+    return {"tokens": tokens_filtered, "entities": entities}
 
 
 def dev():
-    text = "What the BBC thinks about London  and    Mrs Jones."
+    text = "What the BBC thinks about London  and  Barry Jones."
     print(ner(text))
